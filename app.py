d5555049ef3f699f1bf847a9e9c325971106616c
Yusran <60111265+yusrankun@users.noreply.github.com>
2025-08-07 23:24:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import numpy as np
 import joblib
 
 # Load model dan expected columns
-model = joblib.load('best_model_LightGBM.pkl')
-expected_cols = model.named_steps['preprocessor'].feature_names_in_
+model, expected_cols = joblib.load('best_model_LightGBM.pkl')
 
 st.title("Car Price Prediction 🚗")
 
